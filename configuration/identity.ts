c9618532976dfe96d40b21b479fbaba748f33fb3
Norah Binny <89327838+nbbinny@users.noreply.github.com>
2025-03-13 15:08:23 -0400

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Norah`;
 export const OWNER_DESCRIPTION: string = `UNC Chapel Hill student studying computer science`;
 
-export const AI_NAME: string = `RanniBot`;
+export const AI_NAME: string = `Jack`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
