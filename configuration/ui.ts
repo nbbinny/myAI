af88fd50ddcee38fe74711b08297164c1f997fbc
Norah Binny <89327838+nbbinny@users.noreply.github.com>
2025-03-17 17:40:43 -0400

    Update message placeholder

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,7 +1,7 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `HomeScout`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Ask me anything! I'm here to help.`;
 export const FOOTER_MESSAGE: string = `AI By Norah Binny @ UNC Chapel Hill`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
 export const PAGE_TITLE: string = `HomeScout`;
