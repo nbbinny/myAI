d39bddbd49274bceba9a653253d2355be565d36b
Norah Binny <89327838+nbbinny@users.noreply.github.com>
2025-03-17 11:32:03 -0400

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Binny Realty Inc.`;
-export const OWNER_DESCRIPTION: string = `UNC Chapel Hill student studying computer science`;
+export const OWNER_DESCRIPTION: string = `A real estate firm based in Cary, NC.`;
 
 export const AI_NAME: string = `Jack`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_TONE: string = `Friendly`;
+export const AI_ROLE: string = `Real estate agent assistant`;
